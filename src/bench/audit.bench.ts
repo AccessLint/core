6ffb01c6eba807a756a26ba7aea937afff49b420
Cameron Cundiff <cameron@ckundo.com>
2026-02-05 17:38:36 -0500

    Simplify benchmarks to run only against 100 elements

--- src/bench/audit.bench.ts
+++ src/bench/audit.bench.ts
@@ -3,38 +3,30 @@ import { runAudit, rules } from "../rules/index";
 import { clearAriaHiddenCache, clearComputedRoleCache } from "../rules/utils/aria";
 import { clearColorCaches } from "../rules/utils/color";
 import { clearAriaAttrAuditCache } from "../rules/aria/aria-attr-audit";
-import { generateDoc, SMALL_SIZE, MEDIUM_SIZE, LARGE_SIZE } from "./fixtures";
+import { clearSelectorCache } from "../rules/utils/selector";
+import { generateDoc, SMALL_SIZE } from "./fixtures";
 
-const smallDoc = generateDoc(SMALL_SIZE);
-const mediumDoc = generateDoc(MEDIUM_SIZE);
-const largeDoc = generateDoc(LARGE_SIZE);
+const doc = generateDoc(SMALL_SIZE);
 
 function clearCaches() {
   clearAriaHiddenCache();
   clearComputedRoleCache();
   clearColorCaches();
   clearAriaAttrAuditCache();
+  clearSelectorCache();
 }
 
 describe("runAudit", () => {
   bench("100 elements", () => {
-    runAudit(smallDoc);
+    runAudit(doc);
   });
-
-  bench("1k elements", () => {
-    runAudit(mediumDoc);
-  });
-
-  bench("2k elements", () => {
-    runAudit(largeDoc);
-  }, { time: 1000 });
 });
 
-describe("per-rule (1k elements)", () => {
+describe("per-rule (100 elements)", () => {
   for (const rule of rules) {
     bench(rule.id, () => {
       clearCaches();
-      rule.run(mediumDoc);
+      rule.run(doc);
     });
   }
 });
